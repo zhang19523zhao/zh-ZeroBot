e770b5707d8255b6096b43dbf356aab95d5fb9b7
Your Name <you@example.com>
2023-02-13 10:41:29 +0800

    增加过滤关键词

--- example/command/main.go
+++ example/command/main.go
@@ -5,18 +5,31 @@ import (
 	zero "github.com/zhang19523zhao/zh-ZeroBot"
 	"github.com/zhang19523zhao/zh-ZeroBot/config"
 	"github.com/zhang19523zhao/zh-ZeroBot/example/gpt"
+	"github.com/zhang19523zhao/zh-ZeroBot/filter"
 	"github.com/zhang19523zhao/zh-ZeroBot/message"
 )
 
 func init() {
-	command := config.LoadConfig().Command
-	zero.OnCommand(command).Handle(func(ctx *zero.Ctx) {
+	var (
+		replay string
+		err    error
+	)
+
+	//command := config.LoadConfig().Command
+	conf := config.LoadConfig()
+	zero.OnCommand(conf.Command).Handle(func(ctx *zero.Ctx) {
 
 		//fmt.Println("test", ctx.Event.RawMessage)
 		msg := ctx.Event.RawMessage
-		replay, err := gtp.Completions(msg)
-		if err != nil {
-			fmt.Println("gpt err: ", err)
+		check, fmsg := filter.FilterWord(msg, conf.Filter)
+
+		if check {
+			replay, err = gtp.Completions(msg)
+			if err != nil {
+				fmt.Println("gpt err: ", err)
+			}
+		} else {
+			replay = fmsg
 		}
 		// 发送消息
 		ctx.Send(message.Text(replay))
--- example/regex/main.go
+++ example/regex/main.go
@@ -5,17 +5,26 @@ import (
 	zero "github.com/zhang19523zhao/zh-ZeroBot"
 	"github.com/zhang19523zhao/zh-ZeroBot/config"
 	"github.com/zhang19523zhao/zh-ZeroBot/example/gpt"
+	"github.com/zhang19523zhao/zh-ZeroBot/filter"
 	"github.com/zhang19523zhao/zh-ZeroBot/message"
 )
 
 func init() {
-	regs := config.LoadConfig().Regexp
-	zero.OnRegex(regs).Handle(func(ctx *zero.Ctx) {
-
+	var (
+		err    error
+		replay string
+	)
+	conf := config.LoadConfig()
+	zero.OnRegex(conf.Regexp).Handle(func(ctx *zero.Ctx) {
 		msg := ctx.Event.RawMessage
-		replay, err := gtp.Completions(msg)
-		if err != nil {
-			fmt.Println("gpt err: ", err)
+		check, fmsg := filter.FilterWord(msg, conf.Filter)
+		if check {
+			replay, err = gtp.Completions(msg)
+			if err != nil {
+				fmt.Println("gpt err: ", err)
+			}
+		} else {
+			replay = fmsg
 		}
 		// 发送消息
 		ctx.Send(message.Text(replay))
